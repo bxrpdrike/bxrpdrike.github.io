61dee92149cff4af123f0b4473ee61b6bc904d0c
bxrpdrike <laypanadam@gmail.com>
2024-12-26 21:48:33 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -525,7 +525,7 @@ To wrap it up, I want to say I'm doing amazing, but honestly it all just feels s
 
 But with all that said, I think it'll all be okay, time heals all. I'm moving forward regardless of whether or not I know it, I still feel happy a lot of times even though I may not have reflected on it a lot here. I just wanted to tie up all the negative things neatly and send them off with this letter so I know I addressed them and never have to think about them again. I also want to mention that I don't harbor ill will towards you, I know what you did was wrong and unjustified, but I also know that dwelling on it wont get me anywhere. I'd also be pretty dumb to not admit I've made mistakes as well. So I'll just say that I hope you're doing better as well. 
             
-In any case, this is a bit bittersweet, maybe because I'm finally letting go of it all. This'll be the (actual) final goodbye on my end, because I still think it could have been handled better than that paragraph. And I think with this I can finally, truely move on. Thank you for the good memories Cherune, I really, genuinely wish you the best.
+In any case, this is a bit bittersweet, maybe because I'm finally letting go of it all. This'll be the (actual) final goodbye on my end, because I still think it could have been handled better than that paragraph. And I think with this I can finally, truly move on. Thank you for the good memories Cherune, I really, genuinely wish you the best.
 `
         }
         
